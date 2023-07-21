14f8f56714c73836162f3f3aaf79624a734745dd
taorkon <taorkon.tootta@gmail.com>
2023-07-21 21:02:20 +0300

    lesson 1 task 2 with ternary

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,8 +1,5 @@
 // code
 let number1 = Math.floor(Math.random() * 100);
 
-if (number1 % 2) {
-    console.log(number1, ":Это нечетное число");
-} else {
-    console.log(number1, ":Это четное число");
-}
+
+console.log(number1 % 2 ? number1 + " :Это нечетное число" : number1 + " :Это четное число");
