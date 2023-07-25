fd448602123f34f84372c461a6c3839b8ce74b69
taorkon <taorkon.tootta@gmail.com>
2023-07-25 20:28:22 +0300

    lesson2 task 6

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -6,6 +6,6 @@ let passportWithAddress = {
         city: "LA"
     }
 };
-
+console.log(passportWithAddress.address.city);
 passportWithAddress.address.city = 'Bobryisk';
 console.log(passportWithAddress.address.city);
